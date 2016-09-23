7dfc733d65c23081d282f2d7fc159fdc0e67bddc
Dougly <dgalante11@gmail.com>
2016-09-23 14:43:06 -0400

    light bulb changes colors

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -12,19 +12,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lightBulb: UIImageView!
     
-    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        lightBulb.backgroundColor = UIColor.blue
+        lightBulb.backgroundColor = UIColor.red
         
     }
     
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        print(sender.selectedSegmentIndex)
+        switch sender.selectedSegmentIndex {
+        case 0:
+            lightBulb.backgroundColor = UIColor.red
+            break
+        case 1:
+            lightBulb.backgroundColor = UIColor.yellow
+            break
+        case 2:
+            lightBulb.backgroundColor = UIColor.blue
+            break
+        case 3:
+            lightBulb.backgroundColor = UIColor.green
+            break
+        default:
+            break
+        }
         
     }
     
